9f8026c73887b828b00759b61a4e8b0d9552f9d6
snehil <snehil@192.168.1.9>
2024-03-31 19:49:40 +0530

    adding extra route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter ,RouterProvider} from 'react-router-dom';
+import { createBrowserRouter ,RouterProvider,Route,createRoutesFromElements} from 'react-router-dom';
 import './App.css';
 import Home from './component/Home/Home';
 import Layout from './component/layout';
@@ -6,40 +6,72 @@ import About from './component/About/About';
 import Contact from './component/Contact/Contact';
 import User from './component/User/User';
 import Github,{githubInfoLoader} from './component/Github/Github';
-const appRoute = createBrowserRouter([
-    {
-      path : "/",
-      element : <Layout/>,
-      children: [
-         {
-          path : "",
-          element : <Home/>
-         },
-         {
-          path : "about",
-          element : <About/>
-         },
-         {
-          path : "contact",
-          element : <Contact/>
-         },
-         {
-          path : "user/:userId",
-          element : <User/>
-         },
-         {
-          path : "github",
-          element : <Github/>,
-          loader : githubInfoLoader,
-         }
-      ]
-    }
-])
+import Me from './component/About/Me/Me';
+import AboutLayout from './component/About/AboutLayout';
+
+// const router = createBrowserRouter([
+//     {
+//       path : "/",
+//       element : <Layout/>,
+//       children: [
+//          {
+//           path : "",
+//           element : <Home/>
+//          },
+//          {
+//           path : "about",
+//           element : <AboutLayout/>,
+//           children : [
+//              {
+//               path : "",
+//               element : <About/>
+//              },
+//              {
+//               path : "me",
+//               element : <Me/>
+//              }
+//           ]
+//          },
+//          {
+//           path : "contact",
+//           element : <Contact/>
+//          },
+//          {
+//           path : "user/:userId",
+//           element : <User/>
+//          },
+//          {
+//           path : "github",
+//           element : <Github/>,
+//           loader : githubInfoLoader,
+//          }
+//       ]
+//     }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<AboutLayout />} >
+        <Route path='' element={<About/>}/>
+        <Route path='me' element={<Me/>}/>
+      </Route>
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+          loader={githubInfoLoader}
+          path='github' 
+          element={<Github />}
+       />
+    </Route>
+  )
+)
 
 function App() {
   return (
     <>
-     <RouterProvider router={appRoute}/>
+     <RouterProvider router={router}/>
     </>
   );
 }
---
+++ src/component/About/Me/Me.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Me() {
+  return (
+    <>
+      <div>
+       <div>
+        Name : Snehil
+       </div>
+       <div>
+        Mobile no : 7319887158
+       </div>
+      </div>
+    </>
+  )
+}
+
+export default Me
